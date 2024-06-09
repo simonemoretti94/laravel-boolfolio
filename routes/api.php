b6dc4fbfc7fb03c83e96928e7158a9135fbf8f88
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-09 13:41:46 +0200

    3.6th added contacts api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\ProjectController as ProjectApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LeadController as LeadApi;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('projects' , [ProjectApi::class , 'index']);
 Route::get('projects/{id}' , [ProjectApi::class , 'show']);
+
+Route::post('contacts' , [LeadApi::class , 'store']);
