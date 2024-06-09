b489610740aab97dc98cd99dd6a2b63fb1b61fc8
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-09 13:49:31 +0200

    2.1th mail works

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -4,11 +4,38 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Lead;
+use App\Mail\NewLeadMarkdown;
+use Illuminate\Support\Facades\Mail;
 
 class LeadController extends Controller
 {
     public function store(Request $request)
     {
+
+        // validate user inputs
+        $validator = Validator::make($request->all() , [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required'
+        ]);
+
+        if($validator->fails())
+        {
+            // return an error response
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
+
+        // create a lead into db
+        $newLead = Lead::create($request->all());
+
+        // send email
+        Mail::to('noreply@boolfolio.com')->send(new NewLeadMarkdown($newLead));
+
         return response()->json([
             'success' => true,
             'results' => $request->all(),
